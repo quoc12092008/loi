fca31e3f1d42089a0e4f149f66eb4bf00c80b63f
tbikhung <113664115+quoc12092008@users.noreply.github.com>
2025-09-16 00:38:12 +0700

    Create all.lua

---
+++ all.lua
@@ -0,0 +1,15 @@
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/loi/refs/heads/main/fps.lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/loi/refs/heads/main/fps.3lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/loi/refs/heads/main/fps1.lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/loi/refs/heads/main/fps5.lua'))()
+end)
